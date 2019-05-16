6034d7d3ae4e97e560a217dc9d602f7cf0610357
Nkoyan <yoann.kergall@gmail.com>
2019-05-16 13:56:53 +0000

    add http-foundation

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-$name = $_GET['name'];
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-printf('Hello %s', $name);
+require_once __DIR__ . '/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+$name = $request->query->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->prepare($request)->send();

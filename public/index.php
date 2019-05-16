f0ddba94ef2c29efddfa3df56a1a28b226e3c58a
Nkoyan <yoann.kergall@gmail.com>
2019-05-16 15:33:25 +0000

    add http kernel

--- public/index.php
+++ public/index.php
@@ -2,28 +2,23 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Framework;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__ . '/../src/app.php';
+$routes = include __DIR__.'/../src/app.php';
 
 $context = new RequestContext();
-$context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
-try {
-    extract($matcher->match($context->getPathInfo()));
-    ob_start();
-    include __DIR__ . '/../src/pages/' . $_route . '.php';
-    $response = new Response(ob_get_clean());
-} catch (ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->prepare($request)->send();
--- src/app.php
+++ src/app.php
@@ -1,10 +1,43 @@
 <?php
 
+use App\Controller\LeapYearController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
+$routes->add('hello', new Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => function (Request $request) {
+        return render_template($request);
+    }
+]));
+$routes->add('bye', new Route('/bye', [
+    '_controller' => function (Request $request) {
+        return render_template($request);
+    }
+]));
+$routes->add('leap_year', new Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => [LeapYearController::class, 'index']
+]));
 
 return $routes;
